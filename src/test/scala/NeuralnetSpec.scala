9c40671ca97c4ddfdcf8c7849da8e64b93fdf5ec
Antti Juvonen <akajuvonen@users.noreply.github.com>
2017-09-20 10:52:23 +0300

    Removed unneeded extend (Matchers) from tests

--- src/test/scala/NeuralnetSpec.scala
+++ src/test/scala/NeuralnetSpec.scala
@@ -2,7 +2,7 @@ package com.github.akajuvonen.neuralnet_scala
 
 import org.scalatest._
 
-class NeuralnetSpec extends FlatSpec with Matchers {
+class NeuralnetSpec extends FlatSpec{
   "The Neuralnet object" should "do stuff" in {
     assert(1 === 1)
   }
