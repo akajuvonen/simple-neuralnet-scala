b0d5446f4481b8cee1c4324084ec68434edce281
Antti Juvonen <akajuvonen@users.noreply.github.com>
2017-09-20 10:50:47 +0300

    Removed unnecessary fluff from neuralnet and it's tests

--- src/main/scala/Neuralnet.scala
+++ src/main/scala/Neuralnet.scala
@@ -1,9 +1,6 @@
 package com.github.akajuvonen.neuralnet_scala
 
-object Neuralnet extends Greeting with App {
-  println(greeting)
-}
-
-trait Greeting {
-  lazy val greeting: String = "Hi, this is neuralnet"
+object Neuralnet extends App {
+  // TODO
+  println("Hi, this is neuralnet")
 }
--- src/test/scala/NeuralnetSpec.scala
+++ src/test/scala/NeuralnetSpec.scala
@@ -3,7 +3,7 @@ package com.github.akajuvonen.neuralnet_scala
 import org.scalatest._
 
 class NeuralnetSpec extends FlatSpec with Matchers {
-  "The Neuralnet object" should "say: Hi, this is neuralnet" in {
-    Neuralnet.greeting shouldEqual "Hi, this is neuralnet"
+  "The Neuralnet object" should "do stuff" in {
+    assert(1 === 1)
   }
 }
