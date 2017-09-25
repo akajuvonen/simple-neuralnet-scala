34df886647e97ce8d14032a630c21d46d0330519
Antti Juvonen <akajuvonen@users.noreply.github.com>
2017-09-25 08:05:38 +0300

    Added placeholder for sigmoid tools test

---
+++ src/test/scala/SigmoidToolsSpec.scala
@@ -0,0 +1,9 @@
+package com.github.akajuvonen.neuralnet_scala
+
+import org.scalatest._
+
+class SigmoidToolsSpec extends FlatSpec{
+  "The SigmoidTools object" should "do stuff" in {
+    assert(1 === 1)
+  }
+}
