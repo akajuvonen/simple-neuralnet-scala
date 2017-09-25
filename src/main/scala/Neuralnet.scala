037281025d13f703c030d3fa143b726473bb6084
Antti Juvonen <akajuvonen@users.noreply.github.com>
2017-09-25 07:53:26 +0300

    Added comments to neuralnet

--- src/main/scala/Neuralnet.scala
+++ src/main/scala/Neuralnet.scala
@@ -1,7 +1,11 @@
 package com.github.akajuvonen.neuralnet_scala
 
 object Neuralnet {
+  /* The neuralnet class.
+   * TODO
+   */
   def main(args: Array[String]) {
+    // Prints only this for now
     println("Hi, this is neuralnet")
   }
 }
