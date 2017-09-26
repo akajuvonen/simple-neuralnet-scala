d38acd8f7f8ccfa4fa18e3297503003dc1bd2702
Antti Juvonen <akajuvonen@users.noreply.github.com>
2017-09-26 08:35:33 +0300

    Added  sigmoid function test

--- src/test/scala/SigmoidToolsSpec.scala
+++ src/test/scala/SigmoidToolsSpec.scala
@@ -4,7 +4,15 @@ import org.scalatest._
 
 /** SigmoidTools test class. */
 class SigmoidToolsSpec extends FlatSpec{
-  "The SigmoidTools object" should "do stuff" in {
-    assert(1 === 1)
+  "The SigmoidTools object" should "return appr. zero with small x values for sigmoid function" in {
+    assert(SigmoidTools.sigmoid(-100) < 0.01)
+  }
+
+  it should "return appr. one with high x values for sigmoid function" in {
+    assert(SigmoidTools.sigmoid(100) > 0.99)
+  }
+
+  it should "return 0.5 with x=0 in sigmoid function" in {
+    assert(SigmoidTools.sigmoid(0.0) == 0.5)
   }
 }
