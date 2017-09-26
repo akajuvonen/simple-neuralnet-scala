24eb9c49926cd405748d4d69eb2a789e3e41a7db
Antti Juvonen <akajuvonen@users.noreply.github.com>
2017-09-26 08:36:10 +0300

    Corrected comment in neural net spec test

--- src/test/scala/NeuralnetSpec.scala
+++ src/test/scala/NeuralnetSpec.scala
@@ -3,7 +3,7 @@ package com.github.akajuvonen.neuralnet_scala
 import org.scalatest._
 
 class NeuralnetSpec extends FlatSpec{
-  "The Neuralnet object" should "do stuff" in {
+  "The Neuralnet class" should "do stuff" in {
     assert(1 === 1)
   }
 }
