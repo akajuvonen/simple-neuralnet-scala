c212d27c1d2908b32b7906db3a5ee5914990a7cd
Antti Juvonen <akajuvonen@users.noreply.github.com>
2017-09-26 08:45:21 +0300

    Sigmoid tools better test naming and organization

--- src/test/scala/SigmoidToolsSpec.scala
+++ src/test/scala/SigmoidToolsSpec.scala
@@ -4,16 +4,16 @@ import org.scalatest._
 
 /** SigmoidTools test class. */
 class SigmoidToolsSpec extends FlatSpec{
-  "The SigmoidTools object" should
-  "return appr. zero with small x values for sigmoid function" in {
+  "The SigmoidTools.sigmoid function" should
+  "return appr. zero with small x values" in {
     assert(SigmoidTools.sigmoid(-100) < 0.01)
   }
 
-  it should "return appr. one with high x values for sigmoid function" in {
+  it should "return appr. one with high x values" in {
     assert(SigmoidTools.sigmoid(100) > 0.99)
   }
 
-  it should "return 0.5 with x=0 in sigmoid function" in {
+  it should "return 0.5 with x=0" in {
     assert(SigmoidTools.sigmoid(0.0) == 0.5)
   }
 }
