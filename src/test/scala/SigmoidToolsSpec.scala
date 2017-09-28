4d2d9ee54d2b733c7765aaa51f2a80363a752bfd
Antti Juvonen <akajuvonen@users.noreply.github.com>
2017-09-28 14:11:09 +0300

    Added sigmoid derivative function tests

--- src/test/scala/SigmoidToolsSpec.scala
+++ src/test/scala/SigmoidToolsSpec.scala
@@ -4,16 +4,29 @@ import org.scalatest._
 
 /** SigmoidTools test class. */
 class SigmoidToolsSpec extends FlatSpec{
-  "The SigmoidTools.sigmoid" should
+  "SigmoidTools.sigmoid" should
   "return appr. zero with small x values" in {
     assert(SigmoidTools.sigmoid(-100) < 0.01)
   }
 
-  it should "return appr. one with high x values" in {
+  it should "return appr. one with large x values" in {
     assert(SigmoidTools.sigmoid(100) > 0.99)
   }
 
   it should "return 0.5 with x=0" in {
     assert(SigmoidTools.sigmoid(0.0) == 0.5)
+  }
+
+  "SigmoidTools.sigmoidDerivative" should
+  "return appr. zero with small x values" in {
+    assert(SigmoidTools.sigmoidDerivative(SigmoidTools.sigmoid(-10)) < 0.01)
+  }
+
+  it should "return 0.25 with x = 0" in {
+    assert(SigmoidTools.sigmoidDerivative(SigmoidTools.sigmoid(0)) == 0.25)
+  }
+
+  it should "return appr. zero with large x values" in {
+    assert(SigmoidTools.sigmoidDerivative(SigmoidTools.sigmoid(10)) < 0.01)
   }
 }
