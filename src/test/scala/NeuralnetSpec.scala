482e5e54d02e486bbe7e2e00aae40c9a8f4ff304
Antti Juvonen <akajuvonen@users.noreply.github.com>
2017-09-29 07:34:45 +0300

    Corrected code style to match other files

--- src/test/scala/NeuralnetSpec.scala
+++ src/test/scala/NeuralnetSpec.scala
@@ -2,7 +2,7 @@ package com.github.akajuvonen.neuralnet_scala
 
 import org.scalatest._
 
-class NeuralnetSpec extends FlatSpec{
+class NeuralnetSpec extends FlatSpec {
   "The Neuralnet class" should "do stuff" in {
     assert(1 === 1)
   }
--- src/test/scala/SigmoidToolsSpec.scala
+++ src/test/scala/SigmoidToolsSpec.scala
@@ -3,7 +3,7 @@ package com.github.akajuvonen.neuralnet_scala
 import org.scalatest._
 
 /** SigmoidTools test class. */
-class SigmoidToolsSpec extends FlatSpec{
+class SigmoidToolsSpec extends FlatSpec {
   "SigmoidTools.sigmoid" should
   "return appr. zero with small x values" in {
     assert(SigmoidTools.sigmoid(-100) < 0.01)
