bf339187214e8207ebe4ec535bec0309f0a9cce0
Antti Juvonen <akajuvonen@users.noreply.github.com>
2017-10-06 07:38:30 +0300

    Added training input and output data to neuralnet

--- src/main/scala/Neuralnet.scala
+++ src/main/scala/Neuralnet.scala
@@ -12,7 +12,18 @@ class Neuralnet(hidSize: Int = 4, maxIter: Int = 60000) {
 
 /** Neuralnet object for running from cli etc. */
 object Neuralnet extends App {
-  // Consider this the "main" method 
+  // Consider this the "main" method
+  // Training inputs
+  val trainIn = Vector(
+    Vector(1, 0, 0),
+    Vector(0, 0, 1),
+    Vector(1, 1, 1),
+    Vector(1, 1, 0),
+    Vector(0, 1, 0),
+    Vector(0, 0, 0)
+  )
+  // Training outputs
+  val trainOut = Vector(1, 0, 1, 1, 0, 0)
   val nnet: Neuralnet = new Neuralnet
   nnet.sayHi()
 }
