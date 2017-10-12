0364126a8427863b14450ba2aed8a430c2062ae0
Antti Juvonen <akajuvonen@users.noreply.github.com>
2017-10-12 08:00:09 +0300

    Added weight initialization test

--- src/test/scala/NeuralnetSpec.scala
+++ src/test/scala/NeuralnetSpec.scala
@@ -3,7 +3,15 @@ package com.github.akajuvonen.neuralnet_scala
 import org.scalatest._
 
 class NeuralnetSpec extends FlatSpec {
-  "The Neuralnet class" should "do stuff" in {
-    assert(1 === 1)
+  "Neuralnet" should "init weights randomly" in {
+    val trainIn = Vector(Vector(1, 0), Vector(0, 1))
+    val trainOut = Vector(1, 0)
+    val hidSize = 4
+    val net = new Neuralnet(trainIn, trainOut, hidSize, 60000)
+    // Check the weight matrix sizes
+    assert(net.weights1.length == trainIn(0).length)
+    assert(net.weights1(0).length == hidSize)
+    assert(net.weights2.length == hidSize)
+    assert(net.weights2(0).length == trainOut.length)
   }
 }
