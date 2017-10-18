0103ccc8a467050ce172924fad3c6177771e2225
Antti Juvonen <akajuvonen@users.noreply.github.com>
2017-10-18 08:21:54 +0300

    Added matrix tools library placeholder

---
+++ src/main/scala/MatrixTools.scala
@@ -0,0 +1,8 @@
+package com.github.akajuvonen.neuralnet_scala
+
+/** Matrix help tools used by NeuralNetwork class. */
+object MatrixTools {
+  /** TODO */
+  def matMultiply() =
+    println("test")
+}
