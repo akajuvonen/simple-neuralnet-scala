0bad7ea70f33b361bb3a5eb301cd3cb844e37ac4
Antti Juvonen <akajuvonen@users.noreply.github.com>
2017-10-18 08:24:15 +0300

    Matrix tools tests placeholder added

---
+++ src/test/scala/MatrixToolsSpec.scala
@@ -0,0 +1,10 @@
+package com.github.akajuvonen.neuralnet_scala
+
+import org.scalatest._
+
+/** MatrixTools test class. */
+class MatrixToolsSpec extends FlatSpec {
+  "Matrix tools" should "do stuff" in {
+    assert(true)
+  }
+}
