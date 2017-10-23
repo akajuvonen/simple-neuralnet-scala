e2f3a3fb64b1ee042738face4fd1866d4380547c
Antti Juvonen <akajuvonen@users.noreply.github.com>
2017-10-23 09:16:55 +0300

    Added missing documentation, also split a long line

--- src/main/scala/MatrixTools.scala
+++ src/main/scala/MatrixTools.scala
@@ -2,8 +2,14 @@ package com.github.akajuvonen.neuralnet_scala
 
 /** Matrix help tools used by NeuralNetwork class. */
 object MatrixTools {
-  /** TODO */
-  def multiply(a: Vector[Vector[Double]], b: Vector[Vector[Double]]): Vector[Vector[Double]] = {
+  /** Matrix multiplication method.
+   *
+   * @param a The first matrix to multiply.
+   * @param b The second matrix to multiply.
+   * @return Matrix multiplication result.
+   */
+  def multiply(a: Vector[Vector[Double]], b: Vector[Vector[Double]])
+               : Vector[Vector[Double]] = {
     println("test")
     Vector(Vector(1.0, 2.0))
   }
