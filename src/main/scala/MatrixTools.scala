fc05aad75626d6884488dc21c3b7aa0e20b7f487
Antti Juvonen <akajuvonen@users.noreply.github.com>
2017-10-23 09:11:42 +0300

    Added matrix multiplication test (ignored for now)

--- src/main/scala/MatrixTools.scala
+++ src/main/scala/MatrixTools.scala
@@ -3,6 +3,8 @@ package com.github.akajuvonen.neuralnet_scala
 /** Matrix help tools used by NeuralNetwork class. */
 object MatrixTools {
   /** TODO */
-  def matMultiply() =
+  def multiply(a: Vector[Vector[Double]], b: Vector[Vector[Double]]): Vector[Vector[Double]] = {
     println("test")
+    Vector(Vector(1.0, 2.0))
+  }
 }
--- src/test/scala/MatrixToolsSpec.scala
+++ src/test/scala/MatrixToolsSpec.scala
@@ -4,7 +4,21 @@ import org.scalatest._
 
 /** MatrixTools test class. */
 class MatrixToolsSpec extends FlatSpec {
-  "Matrix tools" should "do stuff" in {
-    assert(true)
+  ignore should "do matrix multiplication" in {
+    val a = Vector(
+      Vector(1.0, 2.0, 3.0),
+      Vector(2.0, 3.0, 4.0)
+    )
+    val b = Vector(
+      Vector(1.0, 2.0),
+      Vector(2.0, 3.0),
+      Vector(3.0, 4.0)
+    )
+    val expected = Vector(
+      Vector(14.0, 22.0),
+      Vector(22.0, 29.0)
+    )
+    val result = MatrixTools.multiply(a, b)
+    assert(result == expected)
   }
 }
