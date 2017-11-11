e5de526877c0f29c1b109e523a07fd5c5236abef
Antti Juvonen <akajuvonen@users.noreply.github.com>
2017-11-11 10:01:24 +0200

    Removed neuralnet random weight init test: nnet training is performed at init, therefore random weight init can't be tested. Also, the weight init method is private (as it should be) will not be unit tested at this time

--- src/test/scala/NeuralnetSpec.scala
+++ src/test/scala/NeuralnetSpec.scala
@@ -3,23 +3,7 @@ package com.github.akajuvonen.neuralnet_scala
 import org.scalatest._
 
 class NeuralnetSpec extends FlatSpec {
-  "Neuralnet" should "init weights randomly" in {
-    val trainIn = Vector(Vector(1, 0), Vector(0, 1))
-    val trainOut = Vector(
-      Vector(1),
-      Vector(0)
-    )
-    val hidSize = 4
-    val net = new Neuralnet(trainIn, trainOut, hidSize, 60000)
-    // Check the weight matrix sizes
-    assert(net.weights1.length == trainIn(0).length)
-    assert(net.weights1(0).length == hidSize)
-    assert(net.weights2.length == hidSize)
-    assert(net.weights2(0).length == trainOut(0).length)
-    // Check that the values are between 0 and 1
-    assert(net.weights1.flatten.max < 1.0)
-    assert(net.weights1.flatten.min > 0.0)
-    assert(net.weights2.flatten.max < 1.0)
-    assert(net.weights2.flatten.min > 0.0)
+  "Neuralnet" should "TODO" in {
+    assert(true)
   }
 }
